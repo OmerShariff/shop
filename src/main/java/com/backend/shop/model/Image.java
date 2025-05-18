24201fdd815c031a2bf5a0225250f727be05bca3
OmerShariff <omershariff17@gmail.com>
2025-05-17 23:29:30 -0700

    Product, Image and Category models have been created and their relationship have been defined

---
+++ src/main/java/com/backend/shop/model/Image.java
@@ -0,0 +1,30 @@
+package com.backend.shop.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Blob;
+@Getter
+@Setter
+@AllArgsConstructor
+
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //uses the database’s native identity mechanism to auto-increment primary keys.
+    private long id;
+
+    private String fielname;
+    private String filetype;
+
+    @Lob
+    private Blob image;
+    private String downloadURL;
+
+    @ManyToOne
+    @JoinColumn(name="product_id")
+    //Will be joined to column of name "product_id" in product table
+    private Product product;
+}
