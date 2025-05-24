05071d5ea682bfb318da0b012d729a58619e50d2
OmerShariff <omershariff17@gmail.com>
2025-05-23 23:58:46 -0700

    No change

--- src/main/java/com/backend/shop/model/service/product/IProductService.java
+++ src/main/java/com/backend/shop/model/service/product/IProductService.java
@@ -4,9 +4,6 @@ import com.backend.shop.model.Product;
 
 import java.util.List;
 
-public interface IProductService {
-    Product addProduct(Product product);
-    List<Product> getAllProducts();
-    Product getProductId(Long id);
+
 
 }
