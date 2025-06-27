38b51f45d7bdf015998becaac381582957e53fa1
OmerShariff <omershariff17@gmail.com>
2025-06-26 17:54:16 -0700

    Sql Server created, tested sample tables in console

--- src/main/java/com/backend/shop/ShopApplication.java
+++ src/main/java/com/backend/shop/ShopApplication.java
@@ -1,4 +1,4 @@
-package com.backend.shop;
+package com.backend.shop.model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShopApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ShopApplication.class, args);
 	}
 
