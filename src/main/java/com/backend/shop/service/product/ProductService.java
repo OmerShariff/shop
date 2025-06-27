0c1fb09ff63a1fad4c45605f8526d50e17dde286
OmerShariff <omershariff17@gmail.com>
2025-06-26 18:02:21 -0700

    .

--- src/main/java/com/backend/shop/service/product/ProductService.java
+++ src/main/java/com/backend/shop/service/product/ProductService.java
@@ -1,6 +1,60 @@
 package com.backend.shop.service.product;
 
-public class ProductService {
+import com.backend.shop.model.Product;
+
+import java.util.List;
+
+public class ProductService implements IProductService {
+    private ProductRepository productRepository;
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product getProductId(Long id) {
+        return null;    
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+
+    }
+
+    @Override
+    public void updateProduct(Product product, Long productId) {
+
+    }
+
+    @Override
+    public List<Product> getAllProduct() {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductByCategory(String category) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductByCategoryAndBrand(String category, String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductByBrandAndName(String category, String name) {
+        return List.of();
+    }
+
+    @Override
+    public Long countProductByBrandAndName(String brand, String name) {
+        return 0L;
+    }
 }
 
 //
